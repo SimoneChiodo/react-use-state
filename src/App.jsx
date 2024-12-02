29ad5a2009eb0da03aa9907b2fbe0ad0bb72b274
SimoneChiodo <simochi.005@gmail.com>
2024-12-02 16:05:38 +0100

    import bootstrap; create languages list; create button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
 import { useState } from "react";
+
+// Components
+import Button from "./components/Button";
+
+// Bootstrap CSS
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// Custom CSS
 import "./App.css";
 
 function App() {
     const [count, setCount] = useState(0);
 
+    const linguaggi = [
+        {
+            id: 1,
+            nome: "html",
+            descrizione: "linguaggio 1",
+        },
+        {
+            id: 2,
+            nome: "CSS",
+            descrizione: "linguaggio 2",
+        },
+        {
+            id: 3,
+            nome: "JavaScript",
+            descrizione: "linguaggio 3",
+        },
+        {
+            id: 4,
+            nome: "Node.js",
+            descrizione: "linguaggio 4",
+        },
+        {
+            id: 5,
+            nome: "Express",
+            descrizione: "linguaggio 5",
+        },
+        {
+            id: 6,
+            nome: "ReactJS",
+            descrizione: "linguaggio 6",
+        },
+    ];
+
     return (
         <>
             <h1>Hello World!</h1>
+            {linguaggi.map((linguaggio) => (
+                <Button />
+            ))}
         </>
     );
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+        <>
+            <button type="button" class="btn btn-primary m-3">
+                Primary
+            </button>
+        </>
+    );
+}
